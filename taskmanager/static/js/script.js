cb2d4e798cd8efd51102cffd86b1bb97b3def93c
SamMartin92 <samm92@live.ie>
2022-05-26 15:38:13 +0000

    Adds delete task functions and Procfile

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     var sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-    var instances = M.Sidenav.init(elems, options);
+    
+
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n:{done: 'Select'}
+    });
+
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
+    let collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
   });
+
